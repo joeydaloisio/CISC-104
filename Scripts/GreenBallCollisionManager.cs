3447bae15524986ab3b852f12351e2e552d41bb8
joeydaloisio <113139771+joeydaloisio@users.noreply.github.com>
2022-12-10 14:10:08 -0500

    adding the first four folders

---
+++ Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public GameObject redBallPrefab;
+    public GameObject plane;
+
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        if (OtherBall.GetComponent<RedBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Collided With Red");
+
+            // Since the blue ball collided with the yellow ball, we will make a green one.
+            Instantiate(redBallPrefab, plane.transform);
+        }
+    }
+}
